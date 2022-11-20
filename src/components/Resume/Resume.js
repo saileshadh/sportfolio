db55e08877fcecca1b2fa89bfda9ac2792c4180a
Sailesh <saileshadhikari72@gmail.com>
2022-11-20 10:50:55 +0545

    m

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -36,7 +36,7 @@ const Resume = () => {
               <a href="https://github.com/saileshadh">
                 <button className='btn_shadow'>
               
-              Number of Projects:  10+
+              Number of Projects:  13+
             </button></a>
             <hr></hr>
             <div className="btn_shadow">
